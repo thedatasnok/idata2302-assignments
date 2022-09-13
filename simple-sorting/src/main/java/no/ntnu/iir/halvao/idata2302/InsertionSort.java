01c5d3487ddbae8fb97c5523cc58dba27f508fca
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-09-13 10:27:24 +0200

    feat(simple-sorting): implemented insertion sort with a simple test case

--- simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/InsertionSort.java
+++ simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/InsertionSort.java
@@ -1,15 +1,46 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InsertionSort {
   private InsertionSort() {}
 
+  /**
+   * Sorts a list of integers using the insertion sort algorithm.
+   * 
+   * @param numbers a variable list of numbers
+   * 
+   * @return a sorted list of the input numbers
+   */
   public static List<Integer> sort(Integer... numbers) {
     return InsertionSort.sort(List.of(numbers));
   }
 
-  public static List<Integer> sort(Iterable<Integer> numbers) {
-    throw new UnsupportedOperationException();
+  /**
+   * Sorts a list of integers using insertion sort.
+   * 
+   * @param numbers the list of numbers to sort
+   * 
+   * @return a new list with the number in sorted order
+   */
+  public static List<Integer> sort(List<Integer> numbers) {
+    if (numbers.isEmpty()) throw new IllegalArgumentException("Cannot sort an empty list!");
+
+    List<Integer> unsorted = new ArrayList<>(numbers);
+    List<Integer> sorted = new ArrayList<>();
+
+    while (sorted.size() != numbers.size()) {
+      int minimaIndex = 0;
+
+      for (int i = 0; i < unsorted.size(); i++) {
+        if (unsorted.get(i) < unsorted.get(minimaIndex)) minimaIndex = i;
+      }
+
+      sorted.add(unsorted.get(minimaIndex));
+      unsorted.remove(unsorted.get(minimaIndex));
+    }
+
+    return sorted;
   }
 }
--- simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/InsertionSortTests.java
+++ simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/InsertionSortTests.java
@@ -1,5 +1,21 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class InsertionSortTests {
   
+  @Test
+  void sortsListCorrectly() {
+    List<Integer> sorted = InsertionSort.sort(1, 3, 2, 8, 4, 6);
+    assertArrayEquals(new Integer[]{1, 2, 3, 4, 6, 8}, sorted.toArray(Integer[]::new));
+  }
+
+  @Test
+  void cannotSortEmptyList() {
+    assertThrows(IllegalArgumentException.class, () -> InsertionSort.sort());
+    assertThrows(IllegalArgumentException.class, () -> InsertionSort.sort(List.of()));
+  }
 }
