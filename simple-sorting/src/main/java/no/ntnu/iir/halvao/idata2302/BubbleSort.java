dfb2f644b64a97f149fce042bfee38b3ff351f41
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-09-13 09:32:00 +0200

    feat(simple-sorting): implemented bubble sort algorithm with a simple test

--- simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/BubbleSort.java
+++ simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/BubbleSort.java
@@ -1,15 +1,50 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BubbleSort {
   private BubbleSort() {}
 
+  /**
+   * Sorts a list of integers using the bubble sort algorithm.
+   * 
+   * @param numbers a variable list of numbers
+   * 
+   * @return a sorted list of the input numbers
+   */
   public static List<Integer> sort(Integer... numbers) {
     return BubbleSort.sort(List.of(numbers));
   }
 
-  public static List<Integer> sort(Iterable<Integer> numbers) {
-    throw new UnsupportedOperationException("Not yet implemented!");
+  /**
+   * Sorts a list of integers using bubble sort.
+   * 
+   * @param numbers the list of numbers to sort
+   * 
+   * @return a new list with the sorted elements
+   */
+  public static List<Integer> sort(List<Integer> numbers) {
+    if (numbers.isEmpty()) throw new IllegalArgumentException("Cannot sort an empty array!");
+
+    List<Integer> sorted = new ArrayList<>(numbers);
+    boolean isChanged = true;
+
+    while (isChanged) {
+      isChanged = false;
+
+      for (int i = 0; i < numbers.size() - 1; i++) {
+        int left = sorted.get(i);
+        int right = sorted.get(i + 1);
+  
+        if (right < left) {
+          sorted.set(i, right);
+          sorted.set(i + 1, left);
+          isChanged = true;
+        }
+      }
+    }
+
+    return sorted;
   }
 }
--- simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/BubbleSortTests.java
+++ simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/BubbleSortTests.java
@@ -1,5 +1,22 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class BubbleSortTests {
-  
+
+  @Test
+  void sortsListCorrectly() {
+    List<Integer> sorted = BubbleSort.sort(3, 8, 10, 1, 4);
+    assertArrayEquals(new Integer[]{1, 3, 4, 8, 10}, sorted.toArray(Integer[]::new));
+  }
+
+  @Test
+  void cannotSortEmptyList() {
+    assertThrows(IllegalArgumentException.class, () -> BubbleSort.sort());
+    assertThrows(IllegalArgumentException.class, () -> BubbleSort.sort(List.of()));
+  }
+
 }
