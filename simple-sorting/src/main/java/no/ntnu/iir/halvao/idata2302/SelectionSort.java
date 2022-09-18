6df8872c8e815e5e341babaa0e24324f7690448b
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-09-18 17:52:21 +0200

    feat(simple-sorting): implemented selection sort

--- simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/SelectionSort.java
+++ simple-sorting/src/main/java/no/ntnu/iir/halvao/idata2302/SelectionSort.java
@@ -1,15 +1,54 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SelectionSort {
   private SelectionSort() {}
 
+  /**
+   * Sorts a variadic number of integers using the selection sort algorithm.
+   * 
+   * @param numbers a variable list of numbers
+   * 
+   * @return a sorted list of the input numbers
+   */
   public static List<Integer> sort(Integer... numbers) {
     return SelectionSort.sort(List.of(numbers));
   }
 
-  public static List<Integer> sort(Iterable<Integer> numbers) {
-    throw new UnsupportedOperationException("Not yet implemented!");
+  /**
+   * Sorts a list of integers using the selection sort algorithm.
+   * 
+   * @param numbers the list of integers to sort
+   * 
+   * @return a new sorted list with the numbers in sorted order
+   */
+  public static List<Integer> sort(List<Integer> numbers) {
+    if (numbers.isEmpty()) throw new IllegalArgumentException("Cannot sort an empty list!");
+
+    List<Integer> sorted = new ArrayList<>(numbers);
+
+    for (int i = 0; i < sorted.size(); i++) {
+      int current = sorted.get(i); // left side
+      int minimum = sorted.get(i); // right side
+      int minimumIndex = i;
+
+      // find the minimum value along with its index
+      // we start at the current value of i, everything to the
+      // left of the index i should already be in sorted order
+      for (int j = i; j < sorted.size(); j++) {
+        if (sorted.get(j) < minimum) {
+          minimum = sorted.get(j);
+          minimumIndex = j;
+        }
+      }
+
+      // swap the two values in the array
+      sorted.set(i, minimum);
+      sorted.set(minimumIndex, current);
+    }
+
+    return sorted;
   }
 }
--- simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/SelectionSortTests.java
+++ simple-sorting/src/test/java/no/ntnu/iir/halvao/idata2302/SelectionSortTests.java
@@ -1,5 +1,21 @@
 package no.ntnu.iir.halvao.idata2302;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class SelectionSortTests {
   
+  @Test
+  void sortsListCorrectly() {
+    List<Integer> sorted = SelectionSort.sort(1, 3, 2, 8, 4, 6);
+    assertArrayEquals(new Integer[]{1, 2, 3, 4, 6, 8}, sorted.toArray(Integer[]::new));
+  }
+
+  @Test
+  void cannotSortEmptyList() {
+    assertThrows(IllegalArgumentException.class, () -> SelectionSort.sort());
+    assertThrows(IllegalArgumentException.class, () -> SelectionSort.sort(List.of()));
+  }
 }
